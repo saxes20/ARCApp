bfeddd914b9949e9556fb42e7fc32f82a8d74066
Sameer Saxena <sameerdsaxena@gmail.com>
2021-02-28 15:49:13 -0500

    Initial Commit

---
+++ ARCApp/ARCAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARCAppApp.swift
+//  ARCApp
+//
+//  Created by Sameer Saxena on 2/28/21.
+//
+
+import SwiftUI
+
+@main
+struct ARCAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
